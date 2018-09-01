96c196c20298ceb700d181f10184d0c8b6a9333b
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-01 00:53:18 -0600

    one more setup.sh fix

--- IoTivity/artiklight/setup.sh
+++ IoTivity/artiklight/setup.sh
@@ -4,7 +4,7 @@ PROJNAME=${PWD##*/}
 
 MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity/artiklight/SConscript $CURPWD/"
 eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/pi-boards/artiklight/artiklight.json $CURPWD/$PROJNAME.json"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/artik/artiklight/artiklight.json $CURPWD/$PROJNAME.json"
 eval ${MY_COMMAND}
 MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity/artiklight/artiklight.cpp $CURPWD/src/$PROJNAME.cpp"
 eval ${MY_COMMAND}
