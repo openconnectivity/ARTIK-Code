02134d3963b648999150ad87cb6fb9856e1cec3e
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-01 00:49:49 -0600

    fixing setup.sh

--- IoTivity/artiklight/setup.sh
+++ IoTivity/artiklight/setup.sh
@@ -2,11 +2,11 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/artiklight/SConscript $CURPWD/"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity/artiklight/SConscript $CURPWD/"
 eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/artiklight/artiklight.json $CURPWD/$PROJNAME.json"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/pi-boards/artiklight/artiklight.json $CURPWD/$PROJNAME.json"
 eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/artiklight/artiklight.cpp $CURPWD/src/$PROJNAME.cpp"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity/artiklight/artiklight.cpp $CURPWD/src/$PROJNAME.cpp"
 eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/artiklight/artiklight.cpp.gen $CURPWD/src/$PROJNAME.cpp.gen"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity/artiklight/artiklight.cpp.gen $CURPWD/src/$PROJNAME.cpp.gen"
 eval ${MY_COMMAND}
