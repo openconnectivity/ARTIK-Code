a47e0a6a4d82492c97cbffe882c72592dc4ed351
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-02 18:06:39 -0600

    More errors fixed

--- IoTivity-lite/OCFArtikEnviro/setup.sh
+++ IoTivity-lite/OCFArtikEnviro/setup.sh
@@ -4,7 +4,7 @@ PROJNAME=${PWD##*/}
 
 MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/OCFArtikEnviro/Makefile $CURPWD/"
 eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/pi-boards/enviro-phat/OCFArtikEnviro.json $CURPWD/$PROJNAME.json"
+MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/artik/OCFArtikEnviro/OCFArtikEnviro.json $CURPWD/$PROJNAME.json"
 eval ${MY_COMMAND}
 MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/OCFArtikEnviro/OCFArtikEnviro.c $CURPWD/src/$PROJNAME.c"
 eval ${MY_COMMAND}
