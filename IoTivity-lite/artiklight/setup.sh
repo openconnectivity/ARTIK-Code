9dc97f8265852c41425be93709eecde63f47c543
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-27 10:34:27 -0600

    Updates and simplification of scripts and file name change

--- IoTivity-lite/artiklight/setup.sh
+++ IoTivity-lite/artiklight/setup.sh
@@ -2,11 +2,7 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/Makefile $CURPWD/"
-eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/artik/artiklight/artiklight.json $CURPWD/$PROJNAME.json"
-eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c $CURPWD/src/$PROJNAME.c"
-eval ${MY_COMMAND}
-MY_COMMAND="cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c.gen $CURPWD/src/$PROJNAME.c.gen"
-eval ${MY_COMMAND}
+cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/Makefile ${CURPWD}/
+cp ${EXAMPLEPATH}/ARTIK-Code/artik/artiklight/artiklight.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c ${CURPWD}/src/${PROJNAME}.c
+cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
