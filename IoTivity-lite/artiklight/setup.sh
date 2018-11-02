33ea9dec2cccacc80eedcd1a4a51e0097e350961
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-11-02 12:40:39 -0600

    resolved setup.h differences

--- IoTivity-lite/artiklight/setup.sh
+++ IoTivity-lite/artiklight/setup.sh
@@ -3,13 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/Makefile ${CURPWD}/
-<<<<<<< HEAD
-cp ${EXAMPLEPATH}/ARTIK-Code/artik/artiklight/artiklight.json ${CURPWD}/${PROJNAME}.json
-cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c ${CURPWD}/src/${PROJNAME}.c
-cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
-=======
 cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/ARTIK-Code/artik/artiklight/artiklight.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c ${CURPWD}/src/${PROJNAME}.c
-cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight.c.gen ${CURPWD}/src/${PROJNAME}-gen.c
->>>>>>> c62ceaa14c92bcf080a5260e622abf361f34eb2a
+cp ${EXAMPLEPATH}/ARTIK-Code/IoTivity-lite/artiklight/artiklight-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
